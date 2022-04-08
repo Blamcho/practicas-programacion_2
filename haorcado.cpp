151de35b43d88f1a21356ecfc821d28048b49a2d
Blamcho <94477654+Blamcho@users.noreply.github.com>
2022-04-08 03:56:43 -0500

    Add files via upload

---
+++ haorcado.cpp
@@ -0,0 +1,30 @@
+// haorcado
+// Abraham Torres
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+int filas 5;
+int  columnas = 6;
+int palbras = 5;
+int i, j, k;
+string palbra[filas][columnas][gatos][patos][queso][magos][lanza];
+
+
+int main() {
+
+
+
+
+
+	cout << "hola jugaremos un  haorcado con 5 palabras de 5 letras." << endl;
+
+
+
+
+
+
+
+
+
+}
